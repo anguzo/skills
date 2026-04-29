af11085fdb13f5a70bb227815165ef14861c13b4
Andrei Gužovski <anguzo@taltech.ee>
2026-04-29 22:47:03 +0300

    feat: remove pnpm skill

--- meta.ts
+++ meta.ts
@@ -10,7 +10,6 @@ export interface VendorSkillMeta {
  * Repositories to clone as submodules and generate skills from source
  */
 export const submodules = {
-  pnpm: "https://github.com/pnpm/pnpm.io",
   astro: "https://github.com/withastro/docs",
   opentofu: "https://github.com/opentofu/opentofu",
   terragrunt: "https://github.com/gruntwork-io/terragrunt",
